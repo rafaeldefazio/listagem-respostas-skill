f6e0d9ba149af3dcc5de8aa185664de70e22bbef
Rafael Biagioni de Fazio <rafaelbdefazio@gmail.com>
2020-07-03 19:47:14 -0300

    fix

--- __init__.py
+++ __init__.py
@@ -1,15 +1,26 @@
-from mycroft import MycroftSkill, intent_file_handler
-
+from mycroft import intent_file_handler, MycroftSkill, intent_handler
+from mycroft.skills.context import adds_context, removes_context
+import requests
 
 class ListagemRespostas(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+	def __init__(self):
+		MycroftSkill.__init__(self)
+		
+	@intent_file_handler('respostas.listagem.intent')
+	def handle_respostas_listagem(self, message):
 
-    @intent_file_handler('respostas.listagem.intent')
-    def handle_respostas_listagem(self, message):
-        self.speak_dialog('respostas.listagem')
+		r = requests.get('http://localhost:3000/respostas')
+		rn = r.json()
+		
+		for ri in rn:
+
+			self.speak_dialog('proxima.listagem')
+			self.speak_dialog('texto_alternativa.listagem', data=ri)
+			self.speak_dialog('texto_resposta.listagem', data=ri)
+			self.speak_dialog('contagem.listagem', data=ri)
+		
 
 
 def create_skill():
-    return ListagemRespostas()
+	return ListagemRespostas()
 
