68fcb9ffaa0cdd9f8ce26a4a2e86ef8efea7cdf9
Rafael Biagioni de Fazio <rafaelbdefazio@gmail.com>
2020-08-23 17:51:34 -0300

    update

--- __init__.py
+++ __init__.py
@@ -9,15 +9,34 @@ class ListagemRespostas(MycroftSkill):
 	@intent_file_handler('respostas.listagem.intent')
 	def handle_respostas_listagem(self, message):
 
-		r = requests.get('http://localhost:3000/respostas')
-		rn = r.json()
-		
-		for ri in rn:
+		ipAPI = 'http://177.21.53.138:3000'
 
-			self.speak_dialog('proxima.listagem')
-			self.speak_dialog('texto_alternativa.listagem', data=ri)
-			self.speak_dialog('texto_resposta.listagem', data=ri)
-			self.speak_dialog('contagem.listagem', data=ri)
+
+		payload = {'user':'publico','password':'123'}
+		getToken = requests.post(ipAPI + "/login", data=payload)
+
+
+		myToken = getToken.content;
+		headers = {'x-access-token': myToken}
+
+
+		r = requests.get(ipAPI + '/respostas', headers=headers)
+
+		self.speak(r.status_code);
+
+
+		if r.status_code != 200:
+			self.speak('Ocorreu algum problema ao me conectar ao servidor de dados. Por favor, verifique as credenciais e tente novamente.')
+
+		else:
+			rn = r.json()
+
+			for ri in rn:
+
+				self.speak_dialog('proxima.listagem')
+				self.speak_dialog('texto_alternativa.listagem', data=ri)
+				self.speak_dialog('texto_resposta.listagem', data=ri)
+				self.speak_dialog('contagem.listagem', data=ri)
 		
 
 
